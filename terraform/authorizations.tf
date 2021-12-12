cf9ef3b03e5b98fc2f709934f773328c02ba9e47
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2021-12-12 21:51:58 +0100

    added example cloudbuild to private gke master

--- terraform/authorizations.tf
+++ terraform/authorizations.tf
@@ -2,10 +2,9 @@
 resource "google_iap_web_backend_service_iam_binding" "iap_proxy_https_resource_accessor" {
   web_backend_service = google_compute_backend_service.iap_proxy.name
   role                = "roles/iap.httpsResourceAccessor"
-  members = flatten([
-    format("serviceAccount:%s", google_service_account.iap_proxy_accessor.email),
-    var.accessors,
-  ])
+  members             = local.iap_accessors
+
+  depends_on = [google_project_service.cloudbuild]
 }
 
 resource "google_service_account" "iap_proxy_accessor" {
@@ -16,5 +15,20 @@ resource "google_service_account" "iap_proxy_accessor" {
 resource "google_service_account_iam_binding" "iap_proxy_accessor_service_account_token_creator" {
   service_account_id = google_service_account.iap_proxy_accessor.id
   role               = "roles/iam.serviceAccountTokenCreator"
-  members            = var.accessors
+  members            = local.iap_accessors
+}
+
+
+resource "google_project_service" "cloudbuild" {
+  service            = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+locals {
+  iap_accessors = toset(flatten([
+    format("serviceAccount:%s", google_service_account.iap_proxy_accessor.email),
+    format("serviceAccount:%s@cloudbuild.gserviceaccount.com",
+    data.google_project.current.number),
+    var.accessors,
+  ]))
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,53 @@
+resource "google_secret_manager_secret" "simple_iap_policy" {
+  for_each  = local.exports
+  secret_id = format("simple-iap-proxy-%s", each.key)
+
+  replication {
+    automatic = true
+  }
+  depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "simple_iap_policy" {
+  for_each    = local.exports
+  secret      = google_secret_manager_secret.simple_iap_policy[each.key].id
+  secret_data = each.value
+}
+
+data "google_iam_policy" "simple_iap_policy" {
+  binding {
+    role    = "roles/secretmanager.secretAccessor"
+    members = local.iap_accessors
+  }
+}
+
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  for_each    = local.exports
+  project     = google_secret_manager_secret.simple_iap_policy[each.key].project
+  secret_id   = google_secret_manager_secret.simple_iap_policy[each.key].secret_id
+  policy_data = data.google_iam_policy.simple_iap_policy.policy_data
+}
+
+locals {
+  exports = {
+    target-url      = format("https://%s", trimsuffix(google_dns_record_set.iap_proxy.name, "."))
+    audience        = google_iap_client.iap_proxy.client_id
+    service-account = google_service_account.iap_proxy_accessor.email
+  }
+}
+
 output "iap_proxy_command" {
   value = <<EOF
 simple-iap-proxy gke-client \
-  --target-url https://iap-proxy.${trimsuffix(data.google_dns_managed_zone.tld.dns_name, ".")} \
-  --iap-audience ${google_iap_client.iap_proxy.client_id} \
-  --service-account ${google_service_account.iap_proxy_accessor.email} \
+  --target-url ${local.exports.target-url} \
+  --iap-audience ${local.exports.audience} \
+  --service-account ${local.exports.service-account} \
   --key-file server.key \
   --certificate-file server.crt
 EOF
+}
+
+resource "google_project_service" "secretmanager" {
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
 }
